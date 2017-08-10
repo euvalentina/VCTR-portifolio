17ec2ef57ad3fd54d9327973a5472636360fbe9a
allandevil <allanvicente58@gmail.com>
2017-08-10 17:55:31 +0000

    retirado  menu hamburguer

--- php.js
+++ php.js
@@ -15,10 +15,10 @@ app.use(express.static(path.join(__dirname, 'mountain-king-html')));
 
 //var routes = require('./routes/routes.js')(app);
 
-console.log("Te AMO LINDA");
 
 //app.listen(1337);
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
+  console.log("Te AMO LINDA");
   console.log("Chat server listening at", addr.address + ":" + addr.port);
 });
