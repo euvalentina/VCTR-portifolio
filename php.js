e9e07290a7573cbfafb691079a85ca92dc036077
Allan V Ribeiro <allanvicente58@gmail.com>
2017-08-10 13:49:26 +0000

    phpjs atualizado

--- php.js
+++ php.js
@@ -1,11 +1,22 @@
+var bodyParser = require('body-parser');
 var express = require('express');
 var php = require("node-php");
 var path = require("path");
+var http = require('http');
 
 var app = express();
+var server = http.createServer(app);
 
-app.use("/", php.cgi("/"));
+//app.use("/", php.cgi("/"));
 
-app.listen(3010);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'mountain-king-html')));
 
-console.log("Server listening!");
+//var routes = require('./routes/routes.js')(app);
+
+//app.listen(1337);
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+  var addr = server.address();
+  console.log("Chat server listening at", addr.address + ":" + addr.port);
+});
